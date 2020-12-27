64f791fa2df01c24472a4170caa521f7a02887d8
poorviloki <69414402+poorviloki@users.noreply.github.com>
2020-12-27 12:10:50 +0530

    Add files via upload

---
+++ Bucket-list/app.js
@@ -0,0 +1,26 @@
+var bucketList=[];
+var listItem=document.getElementById("list-container")
+
+function addValues(){
+let value=document.getElementById("input-field").value;
+bucketList.push(value);
+showList();
+}
+function showList(){
+listItem.innerHTML="";
+for(let i=0;i<bucketList.length;i++){
+let li=document.createElement("li");
+li.innerHTML=bucketList[i]+`<span><img src="https://i1.faceprep.in/tick-mark.png" class="checked" onclick="remove(`+i+`)"></span>`;
+li.classList="list";
+listItem.appendChild(li);
+}
+}
+function reset(){
+bucketList=[];
+showList();
+}
+
+function remove(i){
+bucketList.splice(i,1);
+showList();
+}
